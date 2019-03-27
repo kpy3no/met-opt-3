aaca03c43510f4795ae1d9d5dacc9f1c05e6d5a0
%40e.kuzmin.2013 <e.kuzmin.2013%40stud.nstu.ru>
2019-03-27 10:33:57 +0700

    working

--- mo_lab3/Form1.cs
+++ mo_lab3/Form1.cs
@@ -44,7 +44,12 @@ namespace mo_lab3
 
         double func(double x1, double x2, double x3, double x4, double x5, double x6) //Расчет значения функция
         {
-            return (c1 * x1) + (c2 * x2) + (c3 * x3) + (c4 * x4) + (c5 * x5) + (c6 * x6);
+            return (c1 * x1 * x2) + (c2 * x2) + (c3 * x3) + (c4 * x4) + (c5 * x5) + (c6 * x6);
+        }
+
+        double func2(double x1, double x2) //Расчет значения функция
+        {
+            return Math.Pow(x1*x1 + x2 - 11, 2) + Math.Pow(x1 + x2*x2 - 7, 2);
         }
 
         void getC() //Считывание коэффициентов
@@ -59,6 +64,7 @@ namespace mo_lab3
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int n = 2;
             bool flag = false; //Было ли найдено решение
             double r, prev = 0;
             double eps = Convert.ToDouble(txtBoxE.Text);
@@ -76,29 +82,17 @@ namespace mo_lab3
 
                 p = new Search(a, b, Convert.ToDouble(textBox2.Text));
                 s.Add(p);
-
-                p = new Search(a, b, Convert.ToDouble(textBox3.Text));
-                s.Add(p);
-
-                p = new Search(a, b, Convert.ToDouble(textBox4.Text));
-                s.Add(p);
-
-                p = new Search(a, b, Convert.ToDouble(textBox5.Text));
-                s.Add(p);
-
-                p = new Search(a, b, Convert.ToDouble(textBox6.Text));
-                s.Add(p);
             }
 
 
-            for(int i = 0; i < 6; i++)
+            for(int i = 0; i < 2; i++)
             {
                 x.Add(1);
             }
 
             getC();
 
-            min = func(x[0], x[1], x[2], x[3], x[4], x[5]);
+            min = func2(x[0], x[1]);
             prev = min;
 
             int k = 0; //Количество итераций
@@ -107,11 +101,11 @@ namespace mo_lab3
             while (k < 1000 && !flag)
             {
                 k++;
-                for(int i = 0; i < 6; i++)
+                for(int i = 0; i < n; i++)
                 {
 
                     x[i] = s[i].iter(); //Итерация поиска максимума или минимума из заданного диапазона с учетом коэффициента при x[i]
-                    min = func(x[0], x[1], x[2], x[3], x[4], x[5]);
+                    min = func2(x[0], x[1]);
 
                     c += 2;
                     if (Math.Abs(prev - min) <= eps) //Если решение найдено, прекращаем поиск
@@ -127,7 +121,7 @@ namespace mo_lab3
             textBox8.Text = "";
             textBox8.AppendText("Количество итераций: " + k + "\n");
             textBox8.AppendText("Количество вычислений: " + c + "\n");
-            textBox8.AppendText("Полученное минимальное значение функции: " + Convert.ToString(func(x[0], x[1], x[2], x[3], x[4], x[5])) + "\n");
+            textBox8.AppendText("Полученное минимальное значение функции: " + Convert.ToString(func2(x[0], x[1])) + "\n");
             textBox8.AppendText("Предыдущее минимальное значение: " + Convert.ToString(prev) + "\n");
 
             for (int i = 0; i < 6; i++)
